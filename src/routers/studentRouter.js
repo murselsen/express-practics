ad260f6e5835e29a3a78f35d198e06f73a3a2925
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-25 13:13:01 +0300

    Fix formatting inconsistencies in studentRouter.js for improved readability

--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -1,31 +1,31 @@
-import { Router } from "express";
-import { getAllStudents, getStudentById } from "../services/students.js";
-import { createResponse } from "../utils/createResponse.js";
+import { Router } from 'express';
+import { getAllStudents, getStudentById } from '../services/students.js';
+import { createResponse } from '../utils/createResponse.js';
 
 const studentRouter = Router();
 studentRouter.use((req, res, next) => {
   console.log(`Request Method: ${req.method}, Request URL: ${req.originalUrl}`);
   next();
 });
-studentRouter.get("/", async (req, res, next) => {
+studentRouter.get('/', async (req, res, next) => {
   try {
     const students = await getAllStudents();
     if (!students || students.length === 0) {
       return res
         .status(404)
-        .json(createResponse(false, "No students found", null, 404));
+        .json(createResponse(false, 'No students found', null, 404));
     }
     res
       .status(200)
       .json(
-        createResponse(true, "Students fetched successfully", students, 200),
+        createResponse(true, 'Students fetched successfully', students, 200)
       );
   } catch (error) {
     next(error);
   }
 });
 
-studentRouter.get("/:studentId", async (req, res, next) => {
+studentRouter.get('/:studentId', async (req, res, next) => {
   const { studentId } = req.params;
   try {
     const students = await getStudentById(studentId);
@@ -33,11 +33,11 @@ studentRouter.get("/:studentId", async (req, res, next) => {
     if (!student) {
       return res
         .status(404)
-        .json(createResponse(false, "Student not found", null, 404));
+        .json(createResponse(false, 'Student not found', null, 404));
     }
     res
       .status(200)
-      .json(createResponse(true, "Student fetched successfully", student, 200));
+      .json(createResponse(true, 'Student fetched successfully', student, 200));
   } catch (error) {
     next(error);
   }
