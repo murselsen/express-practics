912cc5b0af0dcef8d000d324a43e82523ebb8b42
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-25 14:31:56 +0300

    Refactor studentRouter to use controller functions for fetching students

--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -1,46 +1,16 @@
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from '../services/students.js';
-import { createResponse } from '../utils/createResponse.js';
+import {
+  getAllStudentsController,
+  getStudentByIdController,
+} from '../controllers/studentController.js';
 
 const studentRouter = Router();
 studentRouter.use((req, res, next) => {
   console.log(`Request Method: ${req.method}, Request URL: ${req.originalUrl}`);
   next();
 });
-studentRouter.get('/', async (req, res, next) => {
-  try {
-    const students = await getAllStudents();
-    if (!students || students.length === 0) {
-      return res
-        .status(404)
-        .json(createResponse(false, 'No students found', null, 404));
-    }
-    res
-      .status(200)
-      .json(
-        createResponse(true, 'Students fetched successfully', students, 200)
-      );
-  } catch (error) {
-    next(error);
-  }
-});
+studentRouter.get('/', getAllStudentsController);
 
-studentRouter.get('/:studentId', async (req, res, next) => {
-  const { studentId } = req.params;
-  try {
-    const students = await getStudentById(studentId);
-    // const student = students.find((s) => s.id === Number(studentId));
-    if (!student) {
-      return res
-        .status(404)
-        .json(createResponse(false, 'Student not found', null, 404));
-    }
-    res
-      .status(200)
-      .json(createResponse(true, 'Student fetched successfully', student, 200));
-  } catch (error) {
-    next(error);
-  }
-});
+studentRouter.get('/:studentId', getStudentByIdController);
 
 export default studentRouter;
