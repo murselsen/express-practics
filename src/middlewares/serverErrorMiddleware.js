66b7990e08e7bbbdaf0ef8e5853285e151558af6
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-26 13:05:16 +0300

    Enhance server error middleware to handle HttpError instances with specific status and message

--- src/middlewares/serverErrorMiddleware.js
+++ src/middlewares/serverErrorMiddleware.js
@@ -1,8 +1,23 @@
 import { createResponse } from '../utils/createResponse.js';
+import { HttpError } from 'http-errors';
 
 const serverErrorMiddleware = (err, req, res, next) => {
   console.error('Internal Server Error:');
   console.error(err);
+  if (err instanceof HttpError) {
+    res
+      .status(err.status)
+      .json(
+        createResponse(
+          false,
+          err.message || 'An error occurred',
+          null,
+          err.status,
+          err.code || 'HTTP_ERROR'
+        )
+      );
+    return;
+  }
   res
     .status(500)
     .json(
