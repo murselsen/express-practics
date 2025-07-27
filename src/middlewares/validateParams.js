07d3a2af4ff70ebac7ed6b30c45f843865f84a52
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-28 00:38:21 +0300

    Refactor validateParams middleware for improved logging and code clarity

--- src/middlewares/validateParams.js
+++ src/middlewares/validateParams.js
@@ -1,15 +1,23 @@
-import { createResponse } from '../utils/createResponse';
+import { createResponse } from '../utils/createResponse.js';
 
 const validateParams = (req, res, next) => {
   console.log('Validating request parameters:', req.params);
   const params = Object.entries(req.params);
-
-  console.log('Request parameters:', params);
+  params.forEach(([key, value]) => {
+    console.log(`Parameter: ${key}, Value: ${value}`);
+  });
   if (params.length === 0) {
-    console.warn('No parameters provided in the request');
     return res
       .status(422)
-      .json(createResponse(false, 'No parameters provided', null, 422, 'MISSING_PARAMETERS'));
+      .json(
+        createResponse(
+          false,
+          'No parameters provided',
+          null,
+          422,
+          'MISSING_PARAMETERS'
+        )
+      );
   }
   next();
 };
