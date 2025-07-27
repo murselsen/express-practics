b1b6a5b71c0bc815c49b84c6dc8573ad506ba74d
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-27 03:50:55 +0300

    Fix quotation style in index.js for consistency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Mongo DB connection
-import { initMongoDB } from "./db/initMongoDB.js";
+import { initMongoDB } from './db/initMongoDB.js';
 // Server initialization
-import { startServer } from "./server.js";
+import { startServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoDB();
