482b5609bd7504ebe619ee5c779e3569c6206cc4
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-28 00:32:28 +0300

    Enhance validateParams middleware to handle missing request parameters with appropriate error response

--- src/middlewares/validateParams.js
+++ src/middlewares/validateParams.js
@@ -1,7 +1,17 @@
+import { createResponse } from '../utils/createResponse';
+
 const validateParams = (req, res, next) => {
   console.log('Validating request parameters:', req.params);
   const params = Object.entries(req.params);
 
   console.log('Request parameters:', params);
+  if (params.length === 0) {
+    console.warn('No parameters provided in the request');
+    return res
+      .status(422)
+      .json(createResponse(false, 'No parameters provided', null, 422, 'MISSING_PARAMETERS'));
+  }
   next();
 };
+
+export default validateParams;
