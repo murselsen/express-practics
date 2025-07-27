d6b7eeb35d1af2f10a8daa5331f53676a1c0f439
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-28 00:32:32 +0300

    Refactor studentRouter to include validateParams middleware for GET and DELETE routes

--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -1,18 +1,22 @@
 import { Router } from 'express';
 import {
   createStudentController,
+  deleteStudentController,
   getAllStudentsController,
   getStudentByIdController,
 } from '../controllers/studentController.js';
 
+import validateParams from '../middlewares/validateParams.js';
+
 const studentRouter = Router();
-studentRouter.use((req, res, next) => {
-  next();
-});
+
 studentRouter.get('/', getAllStudentsController);
 
-studentRouter.get('/:studentId', getStudentByIdController);
+studentRouter.get('/:studentId', [validateParams, getStudentByIdController]);
 
 studentRouter.post('/', createStudentController);
+
+// studentRouter.delete('/:studentId');
+studentRouter.delete('/:studentId', [validateParams, deleteStudentController]);
 
 export default studentRouter;
