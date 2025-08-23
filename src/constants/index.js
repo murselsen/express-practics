6e8214569ebee32e158286cb95454a7bb208c28a
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-23 13:47:29 +0300

    Add user roles constants for TEACHER and PARENT in index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,9 @@ export const SORT_ORDER = {
 };
 export const FIFTEEN_MINUTES = 15 * 60 * 1000; // 15 minutes in milliseconds
 export const ONE_DAY = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+
+// User roles
+export const ROLES = {
+  TEACHER: 'teacher',
+  PARENT: 'parent',
+};
