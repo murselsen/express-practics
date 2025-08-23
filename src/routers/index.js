78803cebbad2d48476392506746f3bad5b34ebfb
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-23 13:48:05 +0300

    Add authentication middleware to protect student routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 // Routers
 import authRouter from './authRouter.js';
 import studentRouter from './studentRouter.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const appRouters = express.Router();
 
 appRouters.use('/auth', authRouter);
-appRouters.use('/students', studentRouter);
+// Protect students routes with authenticate
+appRouters.use('/students', authenticate, studentRouter);
 
 export default appRouters;
