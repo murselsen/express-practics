4a4e7ae4ac7b3798e4f402cd63ef117d7f5070a3
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-28 02:15:46 +0300

    Add request reset email and reset password routes with validation

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -4,9 +4,16 @@ import {
   logoutUserController,
   registerUserController,
   refreshUserSessionController,
+  requestResetEmailController,
+  resetPasswordController,
 } from '../controllers/authController.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
 
 const router = express.Router();
 
@@ -19,5 +26,14 @@ router.post(
 router.post('/login', validateBody(loginUserSchema), loginUserController);
 router.post('/refresh', refreshUserSessionController);
 router.post('/logout', logoutUserController);
+
+router.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  requestResetEmailController
+);
+
+router.post('/reset-password', validateBody(resetPasswordSchema), resetPasswordController)
+
 
 export default router;
